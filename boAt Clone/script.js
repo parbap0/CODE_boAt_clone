38b2918306721a2a2315fc3a6fffeddf685d5280
Parth <parbap0@gmail.com>
2022-07-09 13:59:04 +0530

    slider buttons added

--- boAt Clone/script.js
+++ boAt Clone/script.js
@@ -4,12 +4,15 @@ const sliderButtonPrev = document.querySelector(".slider-button-prev");
 
 let temp = 1;
 sliderButtonNext.addEventListener("click", function () {
+  //button needs a fix--->>expected to go to image_1 after image_4
   sliderImage.src = `media/slider_${temp + 1}.png`;
   temp++;
   console.log(temp);
 });
 
 sliderButtonPrev.addEventListener("click", function () {
+  //button needs a fix--->>expected to go to image_4 after image_1
+
   sliderImage.src = `media/slider_${temp - 1}.png`;
   temp--;
   console.log(temp - 1);
