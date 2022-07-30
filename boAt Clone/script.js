b38e8d4dbc695c755290b56ce1bb357912a5b119
Parth <parbap0@gmail.com>
2022-07-30 22:24:13 +0530

    updated slider component

--- boAt Clone/script.js
+++ boAt Clone/script.js
@@ -3,17 +3,26 @@ const sliderButtonNext = document.querySelector(".slider-button-next");
 const sliderButtonPrev = document.querySelector(".slider-button-prev");
 
 let temp = 1;
+console.log(temp);
+
 sliderButtonNext.addEventListener("click", function () {
-  //button needs a fix--->>expected to go to image_1 after image_4
-  sliderImage.src = `media/slider_${temp + 1}.png`;
-  temp++;
-  console.log(temp);
+  if (temp <= 3) {
+    sliderImage.src = `media/slider_${temp + 1}.png`;
+    temp++;
+  } else {
+    temp = 0;
+    sliderImage.src = `media/slider_${temp + 1}.png`;
+    temp++;
+  }
 });
 
 sliderButtonPrev.addEventListener("click", function () {
-  //button needs a fix--->>expected to go to image_4 after image_1
-
-  sliderImage.src = `media/slider_${temp - 1}.png`;
-  temp--;
-  console.log(temp - 1);
+  if (temp > 1) {
+    sliderImage.src = `media/slider_${temp - 1}.png`;
+    temp--;
+  } else if ((temp = 1)) {
+    temp = 5;
+    sliderImage.src = `media/slider_${temp - 1}.png`;
+    temp--;
+  }
 });
