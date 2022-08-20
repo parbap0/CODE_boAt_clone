28777b39aea391a5d2865b99688596c1c7c7e945
Parth <parbap0@gmail.com>
2022-08-21 00:24:21 +0530

    built cards from scratch and javascript hover function

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const sliderImage = document.querySelector(".slider-image");
 const sliderButtonNext = document.querySelector(".slider-button-next");
 const sliderButtonPrev = document.querySelector(".slider-button-prev");
+const sliderOrShop = document.getElementById("myDiv");
+const shop = document.querySelector(".shop");
+const cardImages = document.querySelectorAll(".product-image");
 // const flashSale = document.querySelector(".flash-sale");
+// console.log(cardImage[0]);
 
 let temp = 1;
-console.log(temp);
+// console.log(temp);
+console.log(cardImages);
 
 sliderButtonNext.addEventListener("click", function () {
   if (temp <= 3) {
@@ -27,3 +32,19 @@ sliderButtonPrev.addEventListener("click", function () {
     temp--;
   }
 });
+for (const cardImage of cardImages) {
+  cardImage.addEventListener("mouseover", function () {
+    console.log(cardImage.src);
+    console.log(cardImage.src.indexOf(".png"));
+    let index = cardImage.src.indexOf(".png");
+    cardImage.src =
+      cardImage.src.slice(0, index) + 2 + cardImage.src.slice(index);
+    console.log(cardImage.src);
+  });
+  cardImage.addEventListener("mouseout", function () {
+    let index = cardImage.src.indexOf(".png");
+    cardImage.src =
+      cardImage.src.slice(0, index - 1) + cardImage.src.slice(index);
+    console.log(cardImage.src);
+  });
+}
